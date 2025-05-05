e30c3f2549716d1e0eae3eb7a3c240ef5441e247
pt0015@sc.edu.my <pt0015@sc.edu.my>
2025-05-05 14:13:45 +0800

    update

--- resources/views/oas/admin/offer_letter/home.blade.php
+++ resources/views/oas/admin/offer_letter/home.blade.php
@@ -40,7 +40,7 @@
             <form action="{{ route('offer_letter.create',['id'=>Crypt::encrypt($APPLICATION_RECORD_ID)]) }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">{{ __('Upload Offer Letter Here') }} <span class="text-red-500">*</span></label>
-                <input type="file" name="offerLetter" multiple data-max-file-size="5MB" data-max-files="1" data-allow-reorder="true" class="block  text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" accept=".pdf" required>
+                <input type="file" name="offerLetter" multiple data-max-file-size="5MB" data-max-files="1" data-allow-reorder="true" class="block  text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" required>
                 <br>
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ __('button.submit') }}</button>
             </form>
